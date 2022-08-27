ddfb6690bbb4f04b4cbb5b5ab162798b30e39811
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-27 09:23:26 -0500

    displayed movie desc

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,13 +1,29 @@
 import { useSelector } from 'react-redux';
+import { useHistory } from "react-router-dom";
 
 function Details() {
+
+    const history = useHistory();
 
     const details = useSelector(store => store.details);
     const genres = useSelector(store => store.genres);
 
-    return(
+    console.log(details, genres)
+    return (
         <>
-
+            {details.map(detail => (
+                <div>
+                    <p>{detail.title}</p>
+                    <img src={detail.poster} />
+                    <p>{detail.description}</p>
+                </div>
+            ))}
+            {genres.map(genre => (
+                <div>
+                    <p>{genre.name}</p>
+                </div>
+            ))}
+            <button onClick={() => history.push('/')}>Back</button>
         </>
     )
 }
